4a17d2de7ba63347f50cce6130f3e7650da166ef
WROTZ <94730219+WROTZ@users.noreply.github.com>
2025-12-16 16:47:12 +0530

    Update pdf.js

--- server/utils/pdf.js
+++ server/utils/pdf.js
@@ -1,16 +1,19 @@
-
 import puppeteer from "puppeteer";
 
 export const generatePDF = async (html) => {
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   const page = await browser.newPage();
   await page.setContent(html, { waitUntil: "networkidle0" });
 
-  const pdf = await page.pdf({ format: "A4", printBackground: true });
-  await browser.close();
+  const pdf = await page.pdf({
+    format: "A4",
+    printBackground: true,
+  });
 
+  await browser.close();
   return pdf;
 };
+;
