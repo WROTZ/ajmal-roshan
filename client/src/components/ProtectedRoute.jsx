85ed2d9ab77b6afbc3d68532455c33c967fb2a41
WROTZ <94730219+WROTZ@users.noreply.github.com>
2025-12-16 13:41:17 +0530

    Update ProtectedRoute.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,5 +2,6 @@ import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
     const token = localStorage.getItem("token");
-    return token ? children : <Navigate to="/admin" />;
+    if (!token) return <Navigate to="/admin" />;
+    return children;
 }
